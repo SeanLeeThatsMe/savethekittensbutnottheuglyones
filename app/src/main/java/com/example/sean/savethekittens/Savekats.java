983c4a553077004639b675727290b16e174b50ea
Sean Lee <sean.lee.788@gmail.com>
2016-10-17 21:20:20 -0500

    first commit

---
+++ app/src/main/java/com/example/sean/savethekittens/Savekats.java
@@ -0,0 +1,13 @@
+package com.example.sean.savethekittens;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Savekats extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_savekats);
+    }
+}
